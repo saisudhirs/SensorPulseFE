02cbc02743bfde5af4e3b6dbd797a13f43c8900b
Sai Sudhir <saisudhirs@outlook.com>
2023-04-24 20:47:39 +0530

    date and time selector added

--- src/components/DateTimeSelection.tsx
+++ src/components/DateTimeSelection.tsx
@@ -1,46 +1,47 @@
-// import * as React from 'react';
-// import {Dispatch, SetStateAction} from 'react';
-// // import {Dayjs} from 'dayjs';
-// import TextField from '@mui/material/TextField';
-// import {TimePicker} from '@mui/x-date-pickers/TimePicker';
-// import {DesktopDatePicker, LocalizationProvider} from "@mui/x-date-pickers";
-// import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
-// import Grid from '@mui/material/Grid';
-// import Card from '@mui/material/Card'
-// import Typography from '@mui/material/Typography'
-//
-//
-// export default function DateTimeSelection({
-//                                               label,
-//                                               setValue,
-//                                               value
-//                                           }: { label: string, value: Dayjs | null, setValue: Dispatch<SetStateAction<Dayjs | null>> }) {
-//
-//     // const handleChange = (newValue: Dayjs | null) => {
-//     //     setValue(newValue);
-//     // };
-//     //
-//     // return <Card variant={"outlined"} sx={{width: "100%", minWidth: 100, maxWidth: 200, padding: 2}}>
-//     //     <LocalizationProvider dateAdapter={AdapterDayjs}>
-//     //         <Grid container direction={"column"} maxWidth={"200px"} gap={2}>
-//     //             <Typography>{label}</Typography>
-//     //
-//     //             <DesktopDatePicker
-//     //                 label="Date"
-//     //                 inputFormat="DD/MM/YYYY"
-//     //                 value={value}
-//     //                 onChange={handleChange}
-//     //                 renderInput={(params: JSX.IntrinsicAttributes) => <TextField {...params} />}
-//     //             />
-//     //             <TimePicker
-//     //                 label="Time"
-//     //                 value={value}
-//     //                 onChange={handleChange}
-//     //                 renderInput={(params: JSX.IntrinsicAttributes) => <TextField {...params}/>}
-//     //             />
-//     //         </Grid>
-//     //     </LocalizationProvider>
-//     // </Card>
-//
-// }
-export {}
+import * as React from 'react';
+import {Dispatch, SetStateAction} from 'react';
+import {Dayjs} from 'dayjs';
+import TextField from '@mui/material/TextField';
+import {TimePicker} from '@mui/x-date-pickers/TimePicker';
+import {DesktopDatePicker, LocalizationProvider} from "@mui/x-date-pickers";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card'
+import Typography from '@mui/material/Typography'
+
+
+export default function DateTimeSelection({
+                                              label,
+                                              setValue,
+                                              value
+                                          }: { label: string, value: Dayjs | null, setValue: Dispatch<SetStateAction<Dayjs | null>> }) {
+
+    const handleChange = (newValue: Dayjs | null) => {
+        setValue(newValue);
+    };
+
+    // @ts-ignore
+    return <Card variant={"outlined"} sx={{width: "100%", minWidth: 100, maxWidth: 200, padding: 2}}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Grid container direction={"column"} maxWidth={"200px"} gap={2}>
+                <Typography>{label}</Typography>
+
+                <DesktopDatePicker
+                    label="Date"
+                    inputFormat="DD/MM/YYYY"
+                    value={value}
+                    onChange={handleChange}
+                    renderInput={(params: JSX.IntrinsicAttributes) => <TextField {...params} />}
+                />
+                <TimePicker
+                    label="Time"
+                    value={value}
+                    onChange={handleChange}
+                    renderInput={(params: JSX.IntrinsicAttributes) => <TextField {...params}/>}
+                />
+            </Grid>
+        </LocalizationProvider>
+    </Card>
+
+}
+// export {}
